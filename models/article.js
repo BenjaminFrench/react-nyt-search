c0a4f627310364ba4835294d93de7d6e2dae2fbb
Ben French <ben.jminw@gmail.com>
2018-04-06 13:33:36 -0600

    Updating model to not allow duplicate entries.

--- models/article.js
+++ models/article.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const articleSchema = new Schema({
-  title: { type: String, required: true },
+  title: { type: String, required: true , unique: true},
   date: { type: Date, required: true },
   url: { type: String, required: true }
-});
+}, { timestamps: { createdAt: 'created_at' } });
 
 const Article = mongoose.model("Article", articleSchema);
 
