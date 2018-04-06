6f56081bf2c13207dab23b7cfcaf82f03a3f2ef6
Ben French <ben.jminw@gmail.com>
2018-04-06 13:34:49 -0600

    Moving search results into components for each result.

--- client/src/components/searchResults/searchResults.js
+++ client/src/components/searchResults/searchResults.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "./searchResults.css"
 
-import moment from "moment";
+import SingleResult from "./singleResult";
 
 class SearchResults extends Component {
   constructor(props) {
@@ -24,12 +24,7 @@ class SearchResults extends Component {
             {
               this.props.articles.map( article => {
                 return (
-                  <li className="list-group-item d-flex justify-content-between align-items-center">
-                        <div>
-                          <a className="align-middle" href={article.web_url}>{article.headline.main} </a><span className="article-date small"> {moment(article.pub_date).format('MMMM Do YYYY')}</span>
-                        </div>
-                        <button className="btn btn-success ml-3">Save</button>
-                  </li>
+                  <SingleResult getSavedArticles={this.props.getSavedArticles} article={article}/>
                 );
               })
             }
---
+++ client/src/components/searchResults/singleResult.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import moment from "moment";
+import axios from "axios";
+
+class SingleResult extends Component {
+  handleSave = event => {
+    event.preventDefault();
+
+    // post to /api/articles
+    var newArticle =  {
+      title: this.props.article.headline.main,
+      url: this.props.article.web_url,
+      date: this.props.article.pub_date
+    };
+
+    axios.post('/api/articles', newArticle)
+    .then( response => {
+      this.props.getSavedArticles();
+    })
+    .catch( error => console.log(error));
+  }
+
+  render() {
+    return (
+      <div>
+        <li className="list-group-item d-flex justify-content-between align-items-center">
+          <div>
+            <a className="align-middle" href={this.props.article.web_url}>{this.props.article.headline.main} </a><span className="article-date small"> {moment(this.props.article.pub_date).format('MMMM Do YYYY')}</span>
+          </div>
+          <button onClick={this.handleSave} className="btn btn-success ml-3">Save</button>
+        </li>
+      </div>
+    );
+  }
+}
+
+export default SingleResult;
